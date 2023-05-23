fd39c218916e67aa1b85ab76b3d8378bfd9d21d9
Lv Qiushi <3242736843@qq.com>
2023-05-23 10:46:44 +0800

    add prompt
    Former-commit-id: e1d7787e430c39f195344274eaf0cef43702de9e

--- utils/eval.py
+++ utils/eval.py
@@ -6,16 +6,20 @@ def evaluate(predicter, data_iter):
     dice_val = 0.00
     num = 0
 
+    lst = 180
+
     for batch in data_iter:
+        lst -= 1
+        if lst > 0: 
+            continue
         val_inputs, val_labels = (batch["image"], batch["label"])
-        _, _ , n, _, _ = val_inputs.shape
         dice = Dice(ignore_index = 0)
-        for i in range(n):
-            val_outputs = predicter(val_inputs[0,0,:,:,i], val_labels[0,0,:,:,i])
-            cur = dice(torch.tensor(val_outputs, dtype = torch.int), torch.tensor(val_labels[0,0,:,:,i], dtype = torch.int))
-            print("DICE: %.6lf" %cur)
-            dice_val += cur
-            num += 1
+        print(val_inputs[0])
+        val_outputs = predicter(val_inputs[0], val_labels[0][0])
+        cur = dice(torch.tensor(val_outputs, dtype = torch.int), torch.tensor(val_labels[0][0], dtype = torch.int))
+        print("DICE: %.6lf" %cur)
+        dice_val += cur
+        num += 1
         break
         
     return dice_val / float(num)
