6db5dba738a09aa17e17b2a48798ccc8585db03c
frank3215 <chengefei2003@163.com>
2023-06-12 19:49:28 +0800

    create model_module2.py based on model_module.py
    Former-commit-id: eeda5f0ba3b833919dcfd9f53609b05fba47e23e

--- modeling/sam.py
+++ modeling/sam.py
@@ -63,3 +63,59 @@ class SamWithLabel(Sam):
                 }
             )
         return outputs
+
+
+class SamWithGradients(Sam):
+
+    def __init__(
+        self,
+        image_encoder: ImageEncoderViT,
+        prompt_encoder: PromptEncoder,
+        mask_decoder: MaskLabelDecoder,
+        pixel_mean: List[float] = [123.675, 116.28, 103.53],
+        pixel_std: List[float] = [58.395, 57.12, 57.375],
+    ) -> None:
+        super().__init__(image_encoder, prompt_encoder, mask_decoder, pixel_mean, pixel_std)
+
+    # 必须 override forward，因为原本的 forward 没有 gradient
+    def forward(
+        self,
+        batched_input: List[Dict[str, Any]],
+        multimask_output: bool,
+    ) -> List[Dict[str, torch.Tensor]]:
+        input_images = torch.stack([self.preprocess(x["image"]) for x in batched_input], dim=0)
+        image_embeddings = self.image_encoder(input_images)
+
+        outputs = []
+        for image_record, curr_embedding in zip(batched_input, image_embeddings):
+            if "point_coords" in image_record:
+                points = (image_record["point_coords"], image_record["point_labels"])
+            else:
+                points = None
+            sparse_embeddings, dense_embeddings = self.prompt_encoder(
+                points=points,
+                boxes=image_record.get("boxes", None),
+                masks=image_record.get("mask_inputs", None),
+            )
+            low_res_masks, iou_predictions, label_predictions = self.mask_decoder(
+                image_embeddings=curr_embedding.unsqueeze(0),
+                image_pe=self.prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=multimask_output,
+            )
+            masks = self.postprocess_masks(
+                low_res_masks,
+                input_size=image_record["image"].shape[-2:],
+                original_size=image_record["original_size"],
+            )
+            masks = masks > self.mask_threshold
+            outputs.append(
+                {
+                    "masks": masks,
+                    "iou_predictions": iou_predictions,
+                    'label_predictions': label_predictions,
+                    "low_res_logits": low_res_masks,
+                }
+            )
+        return outputs
--- training/sam_with_label/model_module2.py
+++ training/sam_with_label/model_module2.py
@@ -1,16 +1,50 @@
 import lightning.pytorch as pl
-from modeling.build_sam import pretrained_checkpoints, sam_with_label_model_registry
 from .losses import SegmentationLoss
-from third_party.segment_anything.build_sam import sam_model_registry
 import torch.nn as nn
-from .model_module import DiceMetric, iou_func
+from .model_module import iou_func
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from sam_grad.build_sam import sam_model_registry
+from modeling.build_sam import pretrained_checkpoints
+
+class DiceMetric():
+    def __init__(self):
+        self.reset()
+
+    @torch.no_grad()
+    def reset(self):
+        self.dice = torch.zeros((1, ), dtype=torch.float32)
+        self.n = torch.zeros((1,), dtype=torch.float32)
+    
+    @torch.no_grad()
+    def update(self, pred_binary_mask, binary_label, mask_cls):
+        B = pred_binary_mask.shape[0]
+        pred_binary_mask = pred_binary_mask.view(B, -1)
+        binary_label = binary_label.view(B, -1)
+        TP = torch.count_nonzero(pred_binary_mask & binary_label, dim=1)
+        FP = torch.count_nonzero(pred_binary_mask & (~binary_label), dim=1)
+        FN = torch.count_nonzero((~pred_binary_mask) & (binary_label), dim=1)
+        assert TP.shape[0] == B
+        dice = ((2 * TP) / (2 * TP + FP + FN)).cpu()
+        mask_cls = mask_cls.cpu().to(torch.int)
+        for i in range(B):
+            self.dice[0] += dice[i]
+            self.n += 1
+
+    @torch.no_grad()
+    def get_metrics(self, ignore_background=True):
+        avg_dice = self.dice / self.n
+        if (avg_dice != avg_dice).any():
+            print("Warning: nan in avg_dice")
+        mdice = torch.nanmean(avg_dice)
+        return mdice, avg_dice
 
 # create pl.LightningModule named SAMWithInteractiveTraining
 class SAMWithInteractiveTraining(pl.LightningModule):
-    
     # init
     def __init__(self, 
-                 model_type: str = "vit_h",
+                 model_type: str = "vit_b",
                  train_image_encoder: bool = False,
                  train_prompt_encoder: bool = True,
                  dice_loss_coef: float = 1.0,
@@ -31,40 +65,18 @@ class SAMWithInteractiveTraining(pl.LightningModule):
                      "gamma": 2.0,
                  },
                  debug: bool = True):
-        '''
-        Args:
-            model_type: str, model type, one of ["vit_h", "vit_b", "vit_tiny", "vit_small", "vit_base"]
-            train_image_encoder: bool, whether to train image encoder
-            train_prompt_encoder: bool, whether to train prompt encoder
-            dice_loss_coef: float, dice loss coefficient
-            focal_loss_coef: float, focal loss coefficient
-            label_loss_coef: float, label loss coefficient
-            iou_loss_coef: float, iou loss coefficient
-            optimizer_type: str, optimizer type, one of ["AdamW"]
-            optimizer_kwargs: dict, optimizer kwargs
-        '''
         # init superclass
         super().__init__()
 
         # save hyperparameters
         self.save_hyperparameters()
+        self.train_image_encoder = train_image_encoder
+        self.train_prompt_encoder = train_prompt_encoder
+        self.debug = debug
 
         # retrieve pretrained model
         self.pretrained_checkpoint = pretrained_checkpoints[model_type]
-        pretrained_sam = sam_model_registry[model_type](self.pretrained_checkpoint)
-
-        # load state dict from corresponding pretrained state dict
-        self.model, self.encoder_builder = sam_with_label_model_registry[model_type](None, train_image_encoder)
-        pretrained_state_dict = pretrained_sam.state_dict()
-        new_state_dict = self.model.state_dict()
-        for k, v in pretrained_state_dict.items():
-            if not train_image_encoder and k.startswith("image_encoder"):
-                # freeze image encoder
-                continue
-            
-            assert k in new_state_dict.keys()
-            new_state_dict[k] = v
-        self.model.load_state_dict(new_state_dict)
+        self.model = sam_model_registry[model_type](self.pretrained_checkpoint)
 
         # init losses
         self.segmentation_loss = SegmentationLoss(
@@ -77,11 +89,19 @@ class SAMWithInteractiveTraining(pl.LightningModule):
         self.training_dice_metric = DiceMetric()
         self.validation_dice_metric = DiceMetric()
     
-    def get_logits(self, batch):
+    def get_logits(self,
+                   batch: dict,
+                   point_coords: torch.Tensor,
+                   point_labels: torch.Tensor):
         """
         Args:
-            batch: dict, batch of data, should contain keys ["embedding", "label", "mask_cls", "prompt"]
+            batch: dict, batch of data, containing keys ["embedding", "label", "mask_cls", "prompt"]
+            point_coords: a Tensor of dimension (B, N, 2)
+            point_labels: a Tensor of dimension (B, N)
         Returns:
+            batch_mask: a Tensor of dimension (B, H, W)
+            batch_iou: a Tensor of dimension (B, H, W)
+            batch_label: a Tensor of dimension (B, H, W)
         """
         # image encoder training is not implemented
         assert not self.train_image_encoder, "Unimplemented"
@@ -89,8 +109,6 @@ class SAMWithInteractiveTraining(pl.LightningModule):
         B = len(batch['embedding'])
 
         embeddings = batch['embedding']
-
-        batch = batch.copy()
 
         with torch.set_grad_enabled(torch.is_grad_enabled() and self.train_prompt_encoder):
             sparse_embeddings, dense_embeddings = self.model.prompt_encoder(
@@ -113,8 +131,151 @@ class SAMWithInteractiveTraining(pl.LightningModule):
 
         return batch_mask, batch_iou, batch_label
 
+    def get_loss_and_update_metric(self, batch, batch_mask, batch_iou, metric):
+        B = len(batch['embedding'])
+
+        segmentation_loss = 0.0
+        iou_loss = 0.0
+        
+        img_size = 1024
+
+        mask_cls = batch['mask_cls']
+        lowres_labels = torch.nn.functional.interpolate(
+            batch['label'],
+            (img_size // 4, img_size // 4),
+            mode="nearest-exact"
+        )
+        binary_label = (lowres_labels[:, 0] == mask_cls[:, None, None]).to(torch.float)
+        
+        is_foreground_label = mask_cls != 0
+        segmentation_loss, _dice_loss, _focal_loss = self.segmentation_loss(batch_mask[is_foreground_label], binary_label[is_foreground_label])
+        
+        with torch.no_grad():
+            pred_binary_mask = (batch_mask > self.model.mask_threshold).to(torch.long)
+            
+            binary_label = binary_label.to(torch.long)
+            iou = iou_func(pred_binary_mask[is_foreground_label], binary_label[is_foreground_label])
+            assert (iou.shape[0] == B)
+
+            metric.update(pred_binary_mask, binary_label, mask_cls)
+        
+        iou_loss = ((iou - batch_iou) ** 2).mean()
+
+        return segmentation_loss, iou_loss, _dice_loss, _focal_loss
+
+    def training_step(self, batch, batch_idx):
+        # calculate binary_label
+        img_size = 1024
+        mask_cls = batch['mask_cls']
+        lowres_labels = torch.nn.functional.interpolate(
+            batch['label'],
+            (img_size // 4, img_size // 4),
+            mode="nearest-exact"
+        )
+        binary_label = (lowres_labels[:, 0] == mask_cls[:, None, None]).to(torch.long)
+
+        # concatenate x and y coordinates
+        batch = batch.copy()
+        batch['prompt'] = torch.cat((batch['prompt'][0].reshape(-1, 1), batch['prompt'][1].reshape(-1, 1)), dim=1)
+        point_coords = batch['prompt'][:, None, :]
+
+        # mark these points as foreground points
+        point_labels = torch.ones((B, 1), dtype=torch.int, device=self.device)
+
+        ITERATE_OVER = 10
+        loss_sum = None
+        for i in range(ITERATE_OVER):
+            # run SAM on batch
+            batch_mask, batch_iou, batch_label = self.get_logits(batch, point_coords, point_labels)
+            # calculate predicted binary mask
+            pred_binary_mask = (batch_mask > self.model.mask_threshold).to(torch.long)
+
+            # calculate symmetric difference between binary_label and pred_binary_mask
+            symmetric_difference = (binary_label != pred_binary_mask).to(torch.long)
+
+            # randomly pick a nonzero position in symmetric_difference (shaped B*H*W) for each image in batch
+            batch_nonzero_indices = torch.zeros((B, 2), dtype=torch.long, device=self.device)
+            batch_point_label = torch.zeros((B, 1), dtype=torch.long, device=self.device)
+            for i in range(B):
+                # get the symmetric difference for batch i
+                sample = symmetric_difference[i]
+                # list all nonzero indices
+                nonzero_indices = torch.nonzero(sample)
+                if nonzero_indices.shape[0] == 0:
+                    # TODO: what happens if the two masks exactly match?
+                    continue
+                # randomly select an index
+                random_index = torch.randint(0, nonzero_indices.shape[0], (1,))[0]
+                random_position = nonzero_indices[random_index]
+                random_position = random_position.unsqueeze(0)
+                batch_nonzero_indices[i] = random_position
+                x, y = random_position
+                # don't forget to mark foreground/background!
+                batch_point_label[i] = binary_label[i][x][y]
+
+            # pick the corresponding position in binary_label and pred_binary_mask
+
+            # point_coords is a Tensor of dimension B*N*2, where N is the number of points
+            # batch_nonzero_indices if a Tensor of dimension B*2
+            # concatenate batch_nonzero_indices with point_coords to form a Tensor of dimension B*(N+1)*2
+            point_coords = torch.cat((point_coords, batch_nonzero_indices.unsqueeze(1)), dim=1)
+            # point_labels is a Tensor of dimension B*N, where N is the number of points
+            # batch_point_label if a Tensot of dimension B
+            # concatenate point_labels with point_coords to form a Tensor of dimension B*(N+1)
+            point_labels = torch.cat((point_labels, batch_point_label[:, None]), dim=1)
+
+            # batch?
+        
+            # get loss
+            segmentation_loss, iou_loss, _dice_loss, _focal_loss = self.get_loss_and_update_metric(batch, batch_mask, batch_iou, self.training_dice_metric)
+            loss = self.hparams.iou_loss_coef * iou_loss + segmentation_loss
+            if loss_sum is None:
+                loss_sum = loss
+            else:
+                loss_sum += loss
+
+        ##### Logging
+        self.log("train_loss/total_loss", loss)
+        self.log("train_loss/iou_loss", iou_loss)
+        self.log("train_loss/segmentation_loss", segmentation_loss)
+        self.log("train_loss/dice_loss", _dice_loss)
+        self.log("train_loss/focal_loss", _focal_loss)
+        mdice, avg_dice = self.training_dice_metric.get_metrics()
+        self.log("train_Dice/mDice", mdice)
+
+        return loss_sum / ITERATE_OVER
+
+    def on_train_epoch_end(self):
+        self.training_dice_metric.reset()
+
+    def validation_step(self, batch, batch_idx):
+        batch_mask, batch_iou, batch_label = self.get_logits(batch)
+
+        segmentation_loss, iou_loss, label_loss, _dice_loss, _focal_loss = self.get_loss_and_update_metric(batch, batch_mask, batch_iou, self.validation_dice_metric)
+
+        loss = self.iou_loss_coef * iou_loss + self.label_loss_coef * label_loss + segmentation_loss
+
+        self.log("val_loss/total_loss", loss)
+        self.log("val_loss/label_loss", label_loss)
+        self.log("val_loss/iou_loss", iou_loss)
+        self.log("val_loss/segmentation_loss", segmentation_loss)
+        self.log("val_loss/dice_loss", _dice_loss)
+        self.log("val_loss/focal_loss", _focal_loss)
+        return loss
+
+    def on_validation_epoch_end(self):
+        mdice, avg_dice = self.validation_dice_metric.get_metrics()
+        self.log("val_Dice/mDice", mdice)
+        self.validation_dice_metric.reset()
+
+    def configure_optimizers(self):
+        assert self.optimizer_type in ["AdamW"], "Unimplemented"
+
+        if self.optimizer_type == "AdamW":   
+            optimizer = optim.AdamW(self.parameters(), **self.optimizer_kwargs)
+        return optimizer
 
 if __name__ == "__main__":
     # create model
     model = SAMWithInteractiveTraining()
-    print("model:", model)
+    # print("model:", model)
