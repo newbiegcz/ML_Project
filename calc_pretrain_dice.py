ec103c207d4f93286e65d415445ff856521acb37
Lv Qiushi <3242736843@qq.com>
2023-06-16 12:35:07 +0800

    进行了一些修改

--- calc_pretrain_dice.py
+++ calc_pretrain_dice.py
@@ -1,14 +1,103 @@
 import data.dataset
 import model.prompter
 import utils.eval
+from data.dataset import data_files
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import albumentations.pytorch.transforms
 from data.dataset import Dataset2D
 from data.dataset import data_files
+
+from monai.data import (
+    load_decathlon_datalist,
+    set_track_meta,
+    ThreadDataLoader,
+    CacheDataset
+)
+import torch.utils.data as data
+
+from monai.transforms import (
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    ScaleIntensityRanged,
+    Spacingd,
+    EnsureTyped,
+)
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from segment_anything import SamPredictor, sam_model_registry
+
+class Dataset2D(data.Dataset):
+    def __init__(self, files, *, device, transform, dtype=np.float64, first_only=False, compress=False):
+        if first_only:
+            files = files.copy()[:1]
+
+        self.files = files
+        self.device = device
+        self.transform = transform
+        set_track_meta(True)
+        if compress:
+            _default_transform = Compose(
+                [
+                    LoadImaged(keys=["image", "label"], ensure_channel_first=True, dtype=dtype),
+                    ScaleIntensityRanged(keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True, dtype=dtype),
+                    CropForegroundd(keys=["image", "label"], source_key="image", dtype=dtype),
+                    Orientationd(keys=["image", "label"], axcodes="RAS"),
+                    EnsureTyped(keys=["image", "label"], device=self.device, track_meta=False, dtype=dtype),
+                ]
+            )
+        else:
+            _default_transform = Compose(
+                [
+                    LoadImaged(keys=["image", "label"], ensure_channel_first=True, dtype=dtype),
+                    ScaleIntensityRanged(keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True, dtype=dtype),
+                    CropForegroundd(keys=["image", "label"], source_key="image", dtype=dtype),
+                    Orientationd(keys=["image", "label"], axcodes="RAS"),
+                    EnsureTyped(keys=["image", "label"], device=self.device, track_meta=False, dtype=dtype),
+                ]
+            )
+        
+        self.cache = CacheDataset(
+            data=files, 
+            transform=_default_transform, 
+            cache_rate=1.0, 
+            num_workers=4
+        )
+        set_track_meta(False)
+
+        self.data_list = []
+        for d in self.cache:
+            # print(d['image_meta_dict']['filename_or_obj']) ## raw_data\imagesTr\img0035.nii.gz
+            cur_list = []
+            img, label = d['image'][0], d['label'][0]
+            h = img.shape[2]
+            # print(compress, img.shape)
+            for i in range(h):
+                cur_list.append({
+                    "image": img[:, :, i],
+                    "label": label[:, :, i],
+                    "h": i / h
+                })
+            self.data_list.append(cur_list)
+
+    def __len__(self):
+        return len(self.data_list)
+
+    def __getitem__(self, id, idx):
+        ret = self.data_list[id][idx].copy()
+        #print('image shape:', ret['image'].shape)
+        #print('label shape:', ret['label'].shape)
+        if self.transform:
+            t = self.transform(ret)
+            #print(t['image'].shape, t['label'].shape)
+            return t
+        else:
+            return ret
 
 data_files_validation = data_files["validation"]
 raw_dataset_validation = Dataset2D(data_files_validation, device=torch.device('cpu'), transform=None, dtype=np.float32, compress = True)
@@ -26,6 +115,7 @@ def show_points(coords, labels, ax, marker_size=375):
     neg_points = coords[labels==0]
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
     ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)  
+
 
 def predict(image, label):
     image *= 255.00
@@ -58,8 +148,7 @@ def predict(image, label):
     scores = np.zeros(label.shape, dtype = float)
     
 
-    for i in range(1, 2):
-        print(i)
+    for i in range(1, 14):
         mask = np.zeros(label.shape, dtype = np.uint8)
         mask[label == i] = 1
         if (np.max(mask) == 0):
--- utils/eval.py
+++ utils/eval.py
@@ -1,10 +1,41 @@
 import torch
 from torchmetrics import Dice
 
-def evaluate(predicter, data_iter):
+def evaluate(predicter, data):
 
+    c = len(data)
     dice_val = 0.00
-    num = 0
+
+    for i in range(c):
+        dz = len(data.data_list[i])
+        dx, dy = data.data_list[i][0]["label"].shape
+        for j in range(dz):
+            nonzero_indexes = torch.nonzero(data.data_list[i][j]["label"])
+            if nonzero_indexes.shape[0] == 0:
+                continue
+            else:
+                lz = min(lz, j)
+                rz = max(rz, j)
+                x1 = nonzero_indexes[:, 0].min().item()
+                x2 = nonzero_indexes[:, 0].max().item()
+                y1 = nonzero_indexes[:, 1].min().item()
+                y2 = nonzero_indexes[:, 1].max().item()
+
+                lx = min(lx, x1)
+                rx = max(rx, x2)
+                ly = min(ly, y1)
+                ry = max(ry, y2)
+    
+        deltaz = int(float(rz - lz) * 0.1)
+        deltax = int(float(rx - lx) * 0.1)
+        deltay = int(float(ry - ly) * 0.1)
+
+        lz = max(0, lz - deltaz)
+        rz = min(dz - 1, rz + deltaz)
+        lx = max(0, lx - deltax)
+        rx = min(dx - 1, rx + deltax)
+        ly = max(0, ly - deltay)
+        ry = min(dy - 1, ry + deltay)
 
     for batch in data_iter:
         val_inputs, val_labels = (batch["image"], batch["label"])
