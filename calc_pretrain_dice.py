2814a583cec2df80cd1b42e876aad8a399a37baa
Lv Qiushi <3242736843@qq.com>
2023-06-16 13:06:48 +0800

    做了一些修改

--- calc_pretrain_dice.py
+++ calc_pretrain_dice.py
@@ -1,21 +1,15 @@
 import data.dataset
 import model.prompter
 import utils.eval
-from data.dataset import data_files
-import torch
-import numpy as np
-import matplotlib.pyplot as plt
-import albumentations.pytorch.transforms
-from data.dataset import get_dataset_3d
-from data.dataset import data_files
-
+import gc
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from data.dataset import get_dataset_3d
 from segment_anything import SamPredictor, sam_model_registry
 
-data_files_validation = data_files["validation"]
 dataset_3d = get_dataset_3d('validation', crop_roi=True)
+
 model_type = "vit_h"
 model_checkpoint = "checkpoint/sam_vit_h_4b8939.pth"
 
@@ -31,67 +25,62 @@ def show_points(coords, labels, ax, marker_size=375):
     ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
     ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)  
 
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))    
 
 def predict(image, label):
-    image *= 255.00
 
-    image = torch.tensor(image, dtype = torch.uint8)
+    image = image.permute(1, 2, 0).numpy()
+    image *= 255
+    image = np.array(image, dtype = np.uint8)
 
-    x, y = image.shape
-
-    image = image.unsqueeze(2).repeat(1, 1, 3).numpy()
-
-    #cv2.imshow("image", image)
-    #cv2.waitKey(0)
-
-    global image2
-    image2 = image
-    
-    x, y = label.shape
+    #global image2
+    #image2 = image
 
     sam_predictor.set_image(image)
+        
     result = np.zeros(label.shape)
     scores = np.zeros(label.shape, dtype = float)
-    
 
     for i in range(1, 14):
+        #print(i)
         mask = np.zeros(label.shape, dtype = np.uint8)
         mask[label == i] = 1
         if (np.max(mask) == 0):
             continue
-        prompt = Promptor(mask, 1)
+        prompt = Promptor(mask, sam_predictor, 1, True, False)
+        #for j in range(mask.shape[0]):
+        #    for k in range(mask.shape[1]):
+        #        print(mask[j][k], end = ' ')
+        #    print()
+        #print(prompt)
+        msk, score, logit = sam_predictor.predict(**prompt)  
 
-        # print(prompt)
-        masks, score, logits = sam_predictor.predict(**prompt)  
+        x, y, z = msk.shape
+        msk = msk.reshape(y, z)
 
-        x, y, z = masks.shape
-        print(x, y, z)
-        assert(x == 3)
-        for j in range(3):
-            assert(((mask[j] == 0) | (mask[j] == 1)).all())
+        #plt.ion()
 
-        max_id = 2
+        #plt.figure(figsize=(10,10))
+        #plt.imshow(image)
+        #show_points(prompt['point_coords'], prompt['point_labels'], plt.gca())
+        #plt.axis('on')
+        #plt.show()  
 
-        plt.ion()
-
-        plt.figure(figsize=(10,10))
-        plt.imshow(image)
-        show_points(prompt['point_coords'], prompt['point_labels'], plt.gca())
-        plt.axis('on')
-        plt.show()  
-
-
-        cond = (scores < score[max_id]) & masks[max_id]
-        scores[cond] = score[max_id]
+        #print(scores.shape)
+        #print(msk.shape)
+        cond = (scores < score[0]) & msk
+        scores[cond] = score[0]
         result[cond] = i
 
-    global res2
-    res2 = result
+    #global res2
+    #res2 = result
 
-    global ulabel
-    ulabel = label
+    #global ulabel
+    #ulabel = label.numpy()
 
     return result
-
 
 print(utils.eval.evaluate(predict, dataset_3d))
--- model/prompter.py
+++ model/prompter.py
@@ -4,47 +4,59 @@ import numpy as np
 # model/prompter.py
 # 生成 prompt
 # 可能是一个可训练的模块
+def bounding_box(mask):
+	c = np.nonzero(mask)
+	x1 = np.min(c[0])
+	x2 = np.max(c[0])
+	y1 = np.min(c[1])
+	y2 = np.max(c[1])
+	return x1, x2, y1, y2
+
 class Prompter(torch.nn.Module):
-	def forward(self, mask, num = 1):
-		'''mask 是当前器官的一个 mask
-			 0 表示该像素不在当前器官内,1 表示在器官内
-			 返回一个可以直接给 sam.predict 的 dict
-			   表示选择的 prompt'''
+	def forward(self, mask, sam_predictor, num, points = True, box = False):
+		'''
+		Args:
+			mask: segmentation mask
+			sam_predictor: sam_predictor
+			num: number of prompt points
+			points: whether to use points as prompt
+			box: whether to use box as prompt
+
+		Returns:
+			result: prompt result
+
+		'''
 		result = dict()
-		result["multimask_output"] = True
-		if num == 1:
-			dst = cv.distanceTransform(mask, cv.DIST_L2, 3)
-			ind = np.unravel_index(np.argmax(dst, axis = None), dst.shape)
-			result["point_coords"] = np.array([ind])
-			result["point_labels"] = np.array([1])
-		else:
-			x, y = mask.shape
+		result["multimask_output"] = False
+		if points == True:
 			coords = np.zeros((num, 2))
 			labels = np.zeros(num)
-			for i in range(num):
-				if i % 10 == 0:
-					while(True):
-						ax, ay = np.random.rand(2)
-						cx = int(ax * x)
-						cy = int(ay * y)
-						if mask[cx][cy] == 1:
-							break
-					coords[i][0] = cx
-					coords[i][1] = cy
+			dst = cv.distanceTransform(mask, cv.DIST_L2, 3)
+			ind = np.unravel_index(np.argmax(dst, axis = None), dst.shape)
+			coords[0][0] = ind[1]
+			coords[0][1] = ind[0]
+			labels[0] = 1
+			for i in range(1, num):
+				result["point_coords"] = coords
+				result["point_labels"] = labels
+				msk, score, logit = sam_predictor.predict(**result)
+				x, y, z = msk.shape
+				msk = msk.reshape(y, z)
+				dst = cv.distanceTransform(mask ^ msk, cv.DIST_L2, 3)
+				ind = np.unravel_index(np.argmax(dst, axis = None), dst.shape)
+				coords[i][0] = ind[1]
+				coords[i][1] = ind[0]
+				if mask[ind] == 1:
 					labels[i] = 1
 				else:
-					ax, ay = np.random.rand(2)
-					cx = int(ax * x)
-					cy = int(ay * y)
-					coords[i][0] = cx
-					coords[i][1] = cy
-					if mask[cx][cy] == 1:
-						labels[i] = 1
-					else:
-						labels[i] = -1
+					labels[i] = 0
 
 			result["point_coords"] = coords
 			result["point_labels"] = labels
+		else:
+			assert(box == True)
+			x1, x2, y1, y2 = bounding_box(mask)
+			result["box"] = np.array([y1 - np.random.randint(0, 10), x1 - np.random.randint(0, 10), y2 + np.random.randint(0, 10), x2 + np.random.randint(0, 10)])
 
 		return result
 		
